ae69d6aa0cdb04b2507c7118250eb0ded1a0e9df
fugufish <therealfugu@gmail.com>
2019-03-30 12:17:49 -0600

    update event documentations

--- lib/moleculer/service/event.rb
+++ lib/moleculer/service/event.rb
@@ -3,7 +3,7 @@ require_relative "../support"
 module Moleculer
   module Service
     ##
-    # Represents an action
+    # Represents a service event.
     class Event
       include Support
 
@@ -11,6 +11,12 @@ module Moleculer
       #   @return [String] the name of the action
       attr_reader :name
 
+      ##
+      # @param name [String] the name of the action
+      # @param service [Moleculer::Service] the service to which the action belongs
+      # @param method [Symbol] the method which the event calls when executed
+      # @param options [Hash] the method options
+      # TODO: add ability to group events
       def initialize(name, service, method, options = {})
         @name    = name
         @service = service
@@ -19,17 +25,24 @@ module Moleculer
         @options = options
       end
 
-      def execute(data, options={})
-        @service.new.public_send(@method, data, options)
+      ##
+      # Executes the event
+      # @param data [Hash] the event data
+      def execute(data)
+        @service.new.public_send(@method, data)
       end
 
+      ##
+      # @return [Moleculer::Node] the node of the service this event is tied to
       def node
         @service.node
       end
 
+      ##
+      # @return [Hash] a hash representing this event as it would be in JSON
       def as_json
         {
-          name:    name,
+          name: name,
         }
       end
     end
