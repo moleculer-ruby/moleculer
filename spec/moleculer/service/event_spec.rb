3c0a84fa70fdb1ccf4f9c7faf24b7aada29e6e00
fugufish <therealfugu@gmail.com>
2019-03-30 12:22:55 -0600

    all tests passing

---
+++ spec/moleculer/service/event_spec.rb
@@ -0,0 +1,4 @@
+RSpec.describe Moleculer::Service::Event do
+  let(:service) { instance_double(Moleculer::Service::Base) }
+  let(:node) { instance_double(Moleculer::Node) }
+end
--- spec/moleculer/transporters/redis_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe Moleculer::Transporters::Redis do
-  subject {Moleculer::Transporters::Redis}
-
-  it "sets ::NAME" do
-    expect(subject::NAME).to eq "Redis"
-  end
-end
