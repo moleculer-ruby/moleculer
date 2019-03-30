8e1eb78dbbad2e70da0bdd4dbc4a2e714847b283
fugufish <therealfugu@gmail.com>
2019-03-30 17:50:58 -0600

    add module spec

---
+++ spec/moleculer/moleculer_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe Moleculer do
+  let(:broker) { instance_double(Moleculer::Broker) }
+  describe "#call" do
+    it "ensures the broker is running" do
+
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 require "bundler/setup"
 require "moleculer"
+require 'simplecov'
+
 
 RSpec.configure do |config|
+  SimpleCov.start
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
