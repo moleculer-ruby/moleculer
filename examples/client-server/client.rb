c25d08bc2d49b6b05f849a256cd03754959d0120
fugufish <therealfugu@gmail.com>
2019-04-05 17:15:34 -0600

    fix a few rubocop violations

--- examples/client-server/client.rb
+++ examples/client-server/client.rb
@@ -4,8 +4,7 @@ Moleculer.start
 
 Moleculer.wait_for_services("math")
 
-Moleculer.emit("echo.event", {counter: 1})
-
+Moleculer.emit("echo.event", counter: 1)
 
 result = Moleculer.call("math.add", a: 1, b: 2)
 
