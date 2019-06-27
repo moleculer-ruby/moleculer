96bc7314b5e77eaa5dda1a0ccaecc250909f4087
fugufish <therealfugu@gmail.com>
2019-06-27 08:43:22 -0600

    fix: allow discover packets to direct publish

--- lib/moleculer/packets/discover.rb
+++ lib/moleculer/packets/discover.rb
@@ -5,6 +5,19 @@ module Moleculer
     ##
     # Represents a DISCOVER packet
     class Discover < Base
+      def initialize(broker, data)
+        super(broker, data)
+
+        node = HashUtil.fetch(data, :node, nil)
+        @node_id = HashUtil.fetch(data, :node_id, node)
+      end
+
+      def topic
+        if @node_id
+          return "#{super}.#{@node_id}"
+        end
+        super
+      end
     end
   end
 end
