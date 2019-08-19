c6095f9c0d4927ac635191e46774998c1e11aefc
jarod.reid <therealfugu@gmail.com>
2019-08-19 05:49:40 -0600

    invalid action response is now an Exception instead of a standard error as it is designed to fail fast.

--- lib/moleculer/errors/invalid_action_response.rb
+++ lib/moleculer/errors/invalid_action_response.rb
@@ -2,7 +2,7 @@ module Moleculer
   module Errors
     ##
     # Raised when an action does not return a hash
-    class InvalidActionResponse < StandardError
+    class InvalidActionResponse < Exception
       def initialize(response)
         super "Action must return a Hash, instead it returned a #{response.class.name}"
       end
