8190860614bafab3dd4ffad6dfd1cccb35c95021
jarod.reid <therealfugu@gmail.com>
2019-08-19 05:49:59 -0600

    nevermind

--- lib/moleculer/errors/invalid_action_response.rb
+++ lib/moleculer/errors/invalid_action_response.rb
@@ -2,7 +2,7 @@ module Moleculer
   module Errors
     ##
     # Raised when an action does not return a hash
-    class InvalidActionResponse < Exception
+    class InvalidActionResponse < StandardError
       def initialize(response)
         super "Action must return a Hash, instead it returned a #{response.class.name}"
       end
