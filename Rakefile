0ceb5d53dd49a338ebf5c62d81db16fd52895576
jarod.reid <therealfugu@gmail.com>
2019-08-22 06:14:15 -0600

    add protobuf compile task using thor

--- Rakefile
+++ Rakefile
@@ -3,18 +3,14 @@
 require "bundler/gem_tasks"
 require "rspec/core/rake_task"
 
-load "protobuf/tasks/compile.rake"
+require_relative "lib/moleculer/tasks/compile_protobuf"
 
 RSpec::Core::RakeTask.new(:spec)
 
-MOLECULER_JS_VERSION      = "master"
-PROTOBUF_SOURCE_URL       = "https://raw.githubusercontent.com/moleculerjs/moleculer/#{MOLECULER_JS_VERSION}" \
-                            "/src/serializers/proto/packets.proto"
-PROTOBUF_DESTINATION_FILE = "./tmp/protobuf/packets/packets.proto"
+
 
 task default: :spec
 
 task :compile do
-  `mkdir -p ./tmp/protobuf/packets; curl #{PROTOBUF_SOURCE_URL} > #{PROTOBUF_DESTINATION_FILE}`
-  ::Rake::Task["protobuf:compile"].invoke("packets", "./tmp/protobuf", "lib/moleculer/serializers/protobuf")
+  Moleculer::Tasks::CompileProtobuf.start
 end
--- lib/moleculer/serializers/protobuf/packets/packets.pb.rb
+++ lib/moleculer/serializers/protobuf/packets/packets.pb.rb
@@ -5,133 +5,142 @@
 #
 require 'protobuf'
 
-module Packets
-  ::Protobuf::Optionable.inject(self) { ::Google::Protobuf::FileOptions }
+module Moleculer
+  module Serializers
+    module Protobuf
+      module Packets
+        ::Protobuf::Optionable.inject(self) { ::Google::Protobuf::FileOptions }
 
-  ##
-  # Message Classes
-  #
-  class PacketEvent < ::Protobuf::Message; end
-  class PacketRequest < ::Protobuf::Message; end
-  class PacketResponse < ::Protobuf::Message; end
-  class PacketDiscover < ::Protobuf::Message; end
-  class PacketInfo < ::Protobuf::Message
-    class Client < ::Protobuf::Message; end
+        ##
+        # Message Classes
+        #
+        class PacketEvent < ::Protobuf::Message; end
+        class PacketRequest < ::Protobuf::Message; end
+        class PacketResponse < ::Protobuf::Message; end
+        class PacketDiscover < ::Protobuf::Message; end
+        class PacketInfo < ::Protobuf::Message
+          class Client < ::Protobuf::Message; end
 
-  end
+        end
 
-  class PacketDisconnect < ::Protobuf::Message; end
-  class PacketHeartbeat < ::Protobuf::Message; end
-  class PacketPing < ::Protobuf::Message; end
-  class PacketPong < ::Protobuf::Message; end
-  class PacketGossipHello < ::Protobuf::Message; end
-  class PacketGossipRequest < ::Protobuf::Message; end
-  class PacketGossipResponse < ::Protobuf::Message; end
+        class PacketDisconnect < ::Protobuf::Message; end
+        class PacketHeartbeat < ::Protobuf::Message; end
+        class PacketPing < ::Protobuf::Message; end
+        class PacketPong < ::Protobuf::Message; end
+        class PacketGossipHello < ::Protobuf::Message; end
+        class PacketGossipRequest < ::Protobuf::Message; end
+        class PacketGossipResponse < ::Protobuf::Message; end
 
 
-  ##
-  # Message Fields
-  #
-  class PacketEvent
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :event, 3
-    optional :string, :data, 4
-    repeated :string, :groups, 5
-    optional :bool, :broadcast, 6
-  end
+        ##
+        # Message Fields
+        #
+        class PacketEvent
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :event, 3
+          optional :string, :data, 4
+          repeated :string, :groups, 5
+          optional :bool, :broadcast, 6
+        end
 
-  class PacketRequest
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :id, 3
-    optional :string, :action, 4
-    optional :bytes, :params, 5
-    optional :string, :meta, 6
-    optional :double, :timeout, 7
-    optional :int32, :level, 8
-    optional :bool, :metrics, 9
-    optional :string, :parentID, 10
-    optional :string, :requestID, 11
-    optional :bool, :stream, 12
-  end
+        class PacketRequest
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :id, 3
+          optional :string, :action, 4
+          optional :bytes, :params, 5
+          optional :string, :meta, 6
+          optional :double, :timeout, 7
+          optional :int32, :level, 8
+          optional :bool, :metrics, 9
+          optional :string, :parentID, 10
+          optional :string, :requestID, 11
+          optional :bool, :stream, 12
+        end
 
-  class PacketResponse
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :id, 3
-    optional :bool, :success, 4
-    optional :bytes, :data, 5
-    optional :string, :error, 6
-    optional :string, :meta, 7
-    optional :bool, :stream, 8
-  end
+        class PacketResponse
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :id, 3
+          optional :bool, :success, 4
+          optional :bytes, :data, 5
+          optional :string, :error, 6
+          optional :string, :meta, 7
+          optional :bool, :stream, 8
+        end
 
-  class PacketDiscover
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-  end
+        class PacketDiscover
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+        end
 
-  class PacketInfo
-    class Client
-      optional :string, :type, 1
-      optional :string, :version, 2
-      optional :string, :langVersion, 3
+        class PacketInfo
+          class Client
+            optional :string, :type, 1
+            optional :string, :version, 2
+            optional :string, :langVersion, 3
+          end
+
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :services, 3
+          optional :string, :config, 4
+          repeated :string, :ipList, 5
+          optional :string, :hostname, 6
+          optional ::Moleculer::Serializers::Protobuf::Packets::PacketInfo::Client, :client, 7
+          optional :int32, :seq, 8
+        end
+
+        class PacketDisconnect
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+        end
+
+        class PacketHeartbeat
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :double, :cpu, 3
+        end
+
+        class PacketPing
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :int64, :time, 3
+        end
+
+        class PacketPong
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :int64, :time, 3
+          optional :int64, :arrived, 4
+        end
+
+        class PacketGossipHello
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :host, 3
+          optional :int32, :port, 4
+        end
+
+        class PacketGossipRequest
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :online, 3
+          optional :string, :offline, 4
+        end
+
+        class PacketGossipResponse
+          optional :string, :ver, 1
+          optional :string, :sender, 2
+          optional :string, :online, 3
+          optional :string, :offline, 4
+        end
+
+      end
+
     end
 
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :services, 3
-    optional :string, :config, 4
-    repeated :string, :ipList, 5
-    optional :string, :hostname, 6
-    optional ::Packets::PacketInfo::Client, :client, 7
-    optional :int32, :seq, 8
-  end
-
-  class PacketDisconnect
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-  end
-
-  class PacketHeartbeat
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :double, :cpu, 3
-  end
-
-  class PacketPing
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :int64, :time, 3
-  end
-
-  class PacketPong
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :int64, :time, 3
-    optional :int64, :arrived, 4
-  end
-
-  class PacketGossipHello
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :host, 3
-    optional :int32, :port, 4
-  end
-
-  class PacketGossipRequest
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :online, 3
-    optional :string, :offline, 4
-  end
-
-  class PacketGossipResponse
-    optional :string, :ver, 1
-    optional :string, :sender, 2
-    optional :string, :online, 3
-    optional :string, :offline, 4
   end
 
 end
