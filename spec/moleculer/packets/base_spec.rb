5f528dcd1fce31d91c76a3762f75aa7b3231b486
jarod.reid <therealfugu@gmail.com>
2019-10-05 23:57:12 -0600

    fix rubocop violation

--- spec/moleculer/packets/base_spec.rb
+++ spec/moleculer/packets/base_spec.rb
@@ -5,11 +5,11 @@ require_relative "./base_packet_behavior"
 RSpec.describe Moleculer::Packets::Base do
   let(:broker) do
     Moleculer::Broker.new(Moleculer::Configuration.new(
-        node_id:     "test1",
-        services:    [],
-        log_level:   "trace",
-        transporter: "fake://localhost",
-    ))
+                            node_id:     "test1",
+                            services:    [],
+                            log_level:   "trace",
+                            transporter: "fake://localhost",
+                          ))
   end
 
   subject { Moleculer::Packets::Base.new(broker.config) }
@@ -35,7 +35,6 @@ RSpec.describe Moleculer::Packets::Base do
     context "equal packets" do
       subject { Moleculer::Packets::Base.new(broker.config, ver: "4", sender: "not-node") }
       let(:right) { Moleculer::Packets::Base.new(broker.config, ver: "4", sender: "not-node") }
-
 
       it "should be equal" do
         expect(subject).to eq(right)
