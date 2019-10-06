80876e539e6392769baddce7651c6c105f37970f
jarod.reid <therealfugu@gmail.com>
2019-10-05 21:21:44 -0600

    fix protobuf serializer initialization

---
+++ spec/integration/redis_transporter/protobuf_serializer/actions_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "../../integration_spec_helper"
+require_relative "../../../integration/actions_behavior"
+
+
+RSpec.describe "actions" do
+  it_behaves_like "moleculer actions", "redis", :protobuf
+end
