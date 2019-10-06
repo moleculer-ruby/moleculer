9ce8e0cb96b378be1ff4ef264f91d80ca68e22fa
jarod.reid <therealfugu@gmail.com>
2019-10-05 21:33:12 -0600

    move actions spec

--- spec/integration/fake_transporter/actions_spec.rb
+++ spec/integration/fake_transporter/actions_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "../../integration_spec_helper"
+require_relative "../../egration_spec_helper"
 require_relative "../../../integration/actions_behavior"
 
 
 RSpec.describe "actions" do
-  it_behaves_like "moleculer actions", "fake", :protobuf
+  it_behaves_like "moleculer actions", "fake", :json
 end
--- spec/integration/fake_transporter/json_serializer/actions_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../integration_spec_helper"
-require_relative "../../../integration/actions_behavior"
-
-
-RSpec.describe "actions" do
-  it_behaves_like "moleculer actions", "fake", :json
-end
