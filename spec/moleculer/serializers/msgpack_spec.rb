73d4079031103a6f54277402828299dbb67b0d30
Reece Bisel <reecebisel@gmail.com>
2019-10-14 11:33:58 -0600

    add in tests for message pack serializer

---
+++ spec/moleculer/serializers/msgpack_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "../../lib/moleculer/serializers/msgpack"
+require_relative "serializer_behavior"
+
+RSpec.describe Moleculer::Serializers::MsgPack do
+  let(:config) { Moleculer::Configuration.new }
+  subject { described_class.new(config) }
+
+  include_examples "serializer"
+end
