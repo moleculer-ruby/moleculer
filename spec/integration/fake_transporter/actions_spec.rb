0095e426c6652a32f215490d2e806fa25a726b42
jarod.reid <therealfugu@gmail.com>
2019-10-17 08:13:55 -0600

    remove extra newline

--- spec/integration/fake_transporter/actions_spec.rb
+++ spec/integration/fake_transporter/actions_spec.rb
@@ -3,7 +3,6 @@
 require_relative "../../egration_spec_helper"
 require_relative "../../../integration/actions_behavior"
 
-
 RSpec.describe "actions" do
   it_behaves_like "moleculer actions", "fake", :json
 end
