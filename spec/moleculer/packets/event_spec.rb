825c1a7e2480f9986544c2275dbb495c27dacdc0
jarod.reid <therealfugu@gmail.com>
2019-10-28 07:48:38 -0600

    update event packet to new style

--- spec/moleculer/packets/event_spec.rb
+++ spec/moleculer/packets/event_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "./base_packet_behavior"
+require_relative "./targeted_packet_behavior"
 
 RSpec.describe Moleculer::Packets::Event do
   let(:config) { double(Moleculer::Configuration, node_id: "test1") }
@@ -9,10 +10,5 @@ RSpec.describe Moleculer::Packets::Event do
   subject { Moleculer::Packets::Event.new(config, event: "test", data: {}, broadcast: false, node: node) }
 
   include_examples "base packet"
-
-  describe "#topic" do
-    it "includes the node id" do
-      expect(subject.topic).to eq("MOL.EVENT.test1")
-    end
-  end
+  include_examples "targeted packet"
 end
