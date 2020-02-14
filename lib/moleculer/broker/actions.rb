c2f0708099cdc4485caa3f03404cefab3123ace9
jarod.reid <therealfugu@gmail.com>
2020-02-14 13:17:19 -0700

    add actions specs

--- lib/moleculer/broker/actions.rb
+++ lib/moleculer/broker/actions.rb
@@ -9,51 +9,67 @@ module Moleculer
     # Broker actions
     module Actions
       def initialize(*)
-        @pending_requests ||= Concurrent::Hash.new
+        @pending_requests = Concurrent::Hash.new
         super
       end
 
+      ##
+      # @private
       def add_response(response)
         @pending_requests[response.payload[:id]] = response.payload
       end
 
+      ##
+      # Call an action
+      #
+      # @param action_name [String] name of action
+      # @param params [Hash] params of action
+      # @param options [Hash] options of call
       def call(action_name, params = nil, options = {})
         endpoint = get_action_endpoint(action_name, options[:node_id])
         context  = Context.new(params: params, broker: self, endpoint: endpoint, options: options)
-        endpoint.call(context)
-        Timeout.timeout(@options[:request_timeout]) do
-          wait_for_context(context)
-        end
-        process_response(@pending_requests.delete(context.request_id))
+        call_endpoint(endpoint, context)
       rescue TimeoutError
         @pending_requests.delete(context.request_id)
-        raise Errors::RequestTimeoutError, action
-      rescue => e
+        raise Errors::RequestTimeoutError, endpoint
+      rescue StandardError => e
         @pending_requests.delete(context.request_id) if context
         raise e
       end
 
-      def get_action_endpoint(action_name, node_id)
-        endpoint = registry.get_action_endpoint(action_name, node_id)
-        raise Errors::ServiceNotFound, action_name, node_id unless endpoint
-
-        endpoint
-      end
-
+      ##
+      # @private
       def process_response(response)
         raise Errors.recreate_error(response[:error]) unless response[:success]
 
         response[:data]
       end
 
+      ##
+      # @private
       def send_action(endpoint, context)
-        transit.send_action(endpoint, context)
+        @transit.send_action(endpoint, context)
       end
 
       private
 
+      def get_action_endpoint(action_name, node_id)
+        endpoint = @registry.get_action_endpoint(action_name, node_id)
+        raise Errors::ServiceNotFound, action_name, node_id unless endpoint
+
+        endpoint
+      end
+
+      def call_endpoint(endpoint, context)
+        endpoint.call(context)
+        wait_for_context(context)
+        process_response(@pending_requests.delete(context.request_id))
+      end
+
       def wait_for_context(context)
-        sleep 0.1 until @pending_requests[context.request_id]
+        Timeout.timeout(@options[:request_timeout]) do
+          sleep 0.1 until @pending_requests[context.request_id]
+        end
       end
 
     end
---
+++ spec/moleculer/broker/actions_spec.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+RSpec.describe Moleculer::Broker::Base do
+  subject { Moleculer::Broker::Base.new(request_timeout: 0.01) }
+
+  let(:action) do
+    double(
+      Moleculer::Service::Action,
+      node_id: subject.node_id,
+      call:    true,
+      name:    "action",
+    )
+  end
+
+  before :each do
+    allow(subject.registry).to receive(:get_action_endpoint)
+      .and_return(action)
+  end
+
+  describe "#call" do
+    context "normal action call" do
+      before :each do
+        allow(action).to receive(:call) do |context|
+          subject
+            .add_response(double("response", payload: { id: context.request_id, success: true, data: { success: true } }))
+        end
+
+        it "calls the action" do
+          expect(subject.call("test")).to include(success: true)
+        end
+      end
+    end
+
+    context "timeout error" do
+      it "clears the context and re-raises" do
+        expect { subject.call("test") }.to raise_exception(Moleculer::Errors::RequestTimeoutError)
+      end
+    end
+
+    context "any other error" do
+      before :each do
+        allow(action).to receive(:call) do
+          raise StandardError, "broken!"
+        end
+      end
+
+      it "clears the context and re-raises" do
+        expect { subject.call("test") }.to raise_exception(StandardError)
+      end
+    end
+
+    context "response returns an error" do
+      before :each do
+        allow(action).to receive(:call) do |context|
+          subject
+            .add_response(double("response", payload: {
+              id:      context.request_id,
+              success: false,
+              error:   { name: "AnError" },
+            }))
+        end
+      end
+
+      it "raises the result of the error recreation" do
+        error = StandardError.new
+        expect(Moleculer::Errors).to receive(:recreate_error).with(hash_including(name: "AnError")) do
+          error
+        end
+
+        expect { subject.call("test") }.to raise_error(error)
+      end
+    end
+  end
+
+  describe "#process_response" do
+    it "returns the data within the response" do
+      expect(subject.process_response({success: true, data: {foo: "bar"}})).to include(foo: "bar")
+    end
+  end
+
+  describe "#send_action" do
+    let(:transit) { subject.instance_variable_get(:@transit) }
+    let(:endpoint) { double("Endpoint") }
+    let(:context) { double("Context") }
+
+    it "sends the action to the transit" do
+      expect(transit).to receive(:send_action).with(endpoint, context).and_return(true)
+
+      subject.send_action(endpoint, context)
+    end
+  end
+end
