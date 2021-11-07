d2bcf2762788e8818e3917aeb74c020406820e8d
Jarod <therealfugu@gmail.com>
2021-11-06 23:31:01 -0600

    add documentation to transporters module

--- lib/moleculer/transporters.rb
+++ lib/moleculer/transporters.rb
@@ -3,6 +3,12 @@
 require_relative "transporters/fake"
 
 module Moleculer
+  ##
+  # Transporter communicates with other nodes. It transfers events, calls requests
+  # and processes responses …etc. If multiple instances of a service are running on
+  # different nodes then the requests will be load-balanced among them. The whole
+  # communication logic is outside of transporter class. It means that you can
+  # switch between transporters without changing any line of code.
   module Transporters
     def self.get(name)
       case name
