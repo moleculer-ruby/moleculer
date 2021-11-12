c1560a024f38df46cc5dcb7685f684aacf5f7e20
Jarod <therealfugu@gmail.com>
2021-11-12 10:18:59 -0700

    update registry spec

--- spec/moleculer/registry_spec.rb
+++ spec/moleculer/registry_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 RSpec.describe Moleculer::Registry do
   let(:broker) { double(Moleculer::Broker) }
-  let(:local_node) { double(Moleculer::Node::Local) }
+  let(:local_node) { double(Moleculer::Node) }
   let(:context) { double(Moleculer::Context) }
 
   subject do
@@ -15,6 +15,7 @@ RSpec.describe Moleculer::Registry do
     it "should call the local node" do
       expect(local_node).to receive(:call).with("math.add", context)
 
+      # noinspection RubyMismatchedArgumentType
       subject.call("math.add", context)
     end
   end
