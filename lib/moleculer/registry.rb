2d656538d2535ad42fde2134ab58904491d89b91
Jarod <therealfugu@gmail.com>
2021-11-12 08:41:18 -0700

    update registry spec

--- lib/moleculer/registry.rb
+++ lib/moleculer/registry.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
 module Moleculer
+  ##
+  # @private
   class Registry
-    def initialize(opts); end
+    def initialize(broker, local_node)
+      @broker     = broker
+      @local_node = local_node
+    end
+
+    ##
+    # Calls a service action.
+    #
+    # @param [String] endpoint The endpoint of the service.
+    # @param [Moleculer::Context] context The context of the request.
+    #
+    # @return [any] the result of the action.
+    def call(endpoint, context)
+      call_local_node(endpoint, context)
+    end
+
+    ##
+    # Calls a service action on the local node.
+    #
+    # @param [String] endpoint The endpoint of the service.
+    # @param [Moleculer::Context] context The context of the request.
+    def call_local_node(endpoint, context)
+      local_node.call(endpoint, context)
+    end
+
+    private
+
+    attr_reader :broker, :local_node
   end
 end
---
+++ spec/moleculer/registry_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Moleculer::Registry do
+  let(:broker) { double(Moleculer::Broker) }
+  let(:local_node) { double(Moleculer::Node::Local) }
+  let(:context) { double(Moleculer::Context) }
+
+  subject do
+    described_class.new(broker, local_node)
+  end
+
+  describe "#call" do
+    it "should call the local node" do
+      expect(local_node).to receive(:call).with("math.add", context)
+
+      subject.call("math.add", context)
+    end
+  end
+end
