924266ea75a0d1fb145c6fda80fc94452ddf3879
Jarod <therealfugu@gmail.com>
2021-11-11 21:29:16 -0700

    broker can call services

--- lib/moleculer/broker.rb
+++ lib/moleculer/broker.rb
@@ -3,6 +3,7 @@
 require "socket"
 require "securerandom"
 
+require_relative "local_node"
 require_relative "transit"
 require_relative "event_emitter"
 
@@ -11,37 +12,7 @@ module Moleculer
   # The `Broker` is the main component of Moleculer. It handles services, calls actions,
   # emits events and communicates with remote nodes. You must create a `Broker`
   # instance on every node.
-  #
-  # @param options [Hash] The Moleculer broker options.
-  # @option options [String] :nodeID The node ID, defaults to the hostname + pid.
-  # @option options [String] :namespace The Moleculer namespace, defaults to blank.
-  # @option options [String | Hash] :logger The logger options, defaults to `$stdout`.
-  # @option options [String] :transporter The transporter options, defaults to `"fake"`.
   class Broker
-    ##
-    # @private
-    class Options
-      attr_reader :namespace, :node_id, :transporter
-
-      def initialize(opts = {})
-        @namespace   = opts[:namespace] || ""
-        @node_id     = opts[:node_id] || "#{Socket.gethostname}-#{Process.pid}}"
-        @logger      = opts[:logger] || $stdout
-        @transporter = opts[:transporter] || "fake"
-      end
-
-      def logger
-        case @logger
-        when $stdout
-          { io: $stdout, formatter: :color }
-        when String
-          { file_name: @logger }
-        when Hash
-          hash
-        end
-      end
-    end
-
     include SemanticLogger::Loggable
 
     ##
@@ -52,28 +23,75 @@ module Moleculer
     # @return [String] the instance uuid
     attr_reader :instance_id
 
+    ##
+    # @param opts [Hash] The Moleculer broker options.
+    # @option opts [String] :nodeID The node ID, defaults to the hostname + pid.
+    # @option opts [String] :namespace The Moleculer namespace, defaults to blank.
+    # @option opts [String | Hash] :logger The logger options, defaults to `$stdout`.
+    # @option opts [String] :transporter The transporter options, defaults to `"fake"`.
+    # @option opts [String] :services The services to register, defaults to `[]`.
     def initialize(opts = {})
-      @options     = Options.new(opts)
       @started     = false
+      @node_id     = opts[:node_id] || "#{Socket.gethostname}-#{Process.pid}}"
       @instance_id = SecureRandom.uuid
       @local_bus   = EventEmitter.new
-      @transit     = Transit.new(self, Transporters.get(options.transporter).new)
+      @local_node  = LocalNode.new(
+        id:       @node_id,
+        services: (opts[:services] || []),
+      )
 
-      set_semantic_logger(options.logger)
+      set_semantic_logger(opts[:logger] || $stdout)
     end
 
+    ##
+    # Starts the broker.
     def start
       logger.info("Moleculer Ruby v#{Moleculer::VERSION}")
       logger.info("starting")
-      transit.connect
+    end
+
+    ##
+    # Calls a service action.
+    #
+    # @param [String] endpoint The endpoint of the service.
+    # @param [Hash] params The params to pass to the action
+    #
+    # @return [any] the result of the action.
+    def call(endpoint, params = {})
+      call_local_node(endpoint, params)
+    end
+
+    ##
+    # Calls a service action on the local node.
+    #
+    # @param [String] endpoint The endpoint of the service.
+    # @param [Hash] params The params to pass to the action
+    def call_local_node(endpoint, params = {})
+      logger.info("calling '#{endpoint}' locally", params: params)
+      context = Context.new(self, params)
+
+      local_node.call(endpoint, context)
     end
 
     private
 
-    attr_reader :options, :local_bus, :transit
+    attr_reader :options, :local_bus, :transit, :local_node
 
+    # rubocop:disable Naming/AccessorMethodName
     def set_semantic_logger(logger)
-      SemanticLogger.add_appender(**logger)
+      SemanticLogger.add_appender(**log_options(logger))
+    end
+    # rubocop:enable Naming/AccessorMethodName
+
+    def log_options(logger)
+      case logger
+      when $stdout
+        { io: $stdout, formatter: :color }
+      when String
+        { file_name: logger }
+      when Hash
+        hash
+      end
     end
   end
 end
--- spec/moleculer/broker_spec.rb
+++ spec/moleculer/broker_spec.rb
@@ -3,12 +3,30 @@
 require "spec_helper"
 
 RSpec.describe Moleculer::Broker do
+  let(:service) do
+    Class.new(Moleculer::Service) do
+      name "math"
+
+      action "sum", :sum
+
+      def sum(ctx)
+        ctx.params[:a] + ctx.params[:b]
+      end
+    end
+  end
+
+  subject { Moleculer::Broker.new(services: [service]) }
   describe "#initialize" do
     it "should create a broker with defaults" do
-      broker = Moleculer::Broker.new
-      expect(broker).to be_a(Moleculer::Broker)
-      expect(broker.started).to be_falsey
-      expect(broker.instance_id).to_not be_empty
+      expect(subject).to be_a(Moleculer::Broker)
+      expect(subject.started).to be_falsey
+      expect(subject.instance_id).to_not be_empty
+    end
+  end
+
+  describe "#call" do
+    it "should call a service" do
+      expect(subject.call("math.sum", { a: 1, b: 2 })).to eq(3)
     end
   end
 end
