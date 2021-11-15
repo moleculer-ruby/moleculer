873414bb4904bdb43f89a1c83298264c1651e298
Jarod <therealfugu@gmail.com>
2021-11-14 18:04:33 -0700

    fix formatting

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -4,16 +4,16 @@
 # @private
 class String
   def camelize
-    str = split("_").map(&:capitalize).join
+    str    = split("_").map(&:capitalize).join
     str[0] = str[0].downcase
     str.gsub("Id", "ID")
   end
 
   def underscore
-    gsub(/::/, '/').
-    gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
-    gsub(/([a-z\d])([A-Z])/, '\1_\2').
-    tr("-", "_").
-    downcase
+    gsub(/::/, "/")
+      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+      .tr("-", "_")
+      .downcase
   end
 end
